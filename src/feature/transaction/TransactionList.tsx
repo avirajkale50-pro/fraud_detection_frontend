9c338c47d9339da7162c9c185a578184640eb462
avirajkale50-pro <aviraj.kale@joshsoftware.com>
2026-01-27 16:34:43 +0530

    The logo changes is done and the transaction badge is fixed

--- src/feature/transaction/TransactionList.tsx
+++ src/feature/transaction/TransactionList.tsx
@@ -127,6 +127,7 @@ const TransactionList: React.FC = () => {
                                 <div className="flex items-center gap-4">
                                     <div className="text-right space-y-2">
                                         <RiskBadge score={transaction.risk_score} />
+                                        <br />
                                         <DecisionBadge decision={transaction.decision} />
                                     </div>
                                     <ArrowRight className="w-5 h-5 text-gray-400 group-hover:text-gray-600 group-hover:translate-x-1 transition-all" />
